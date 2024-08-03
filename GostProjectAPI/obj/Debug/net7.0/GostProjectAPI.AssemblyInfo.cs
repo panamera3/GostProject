59e5f8e2de45a6161d58cc4f0a5971f1f2d8f28a
Lena <pjanvalena333@gmail.com>
2024-08-03 05:19:45 +0500

    fix

--- GostProjectAPI/obj/Debug/net7.0/GostProjectAPI.AssemblyInfo.cs
+++ GostProjectAPI/obj/Debug/net7.0/GostProjectAPI.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("GostProjectAPI")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3160d23fecd73ab62ed0d1d7b331ff4ca44caa36")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9bf2202dcfa5312a09794096fd1357dff970a2b1")]
 [assembly: System.Reflection.AssemblyProductAttribute("GostProjectAPI")]
 [assembly: System.Reflection.AssemblyTitleAttribute("GostProjectAPI")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
